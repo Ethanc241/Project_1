a79bd6aacf0a4daf970da8705ae35bcff7f9a50c
ethan <ethanc241@nycstudents.net>
2025-09-30 02:34:19 -0400

    Initial/final commit. All in oneeee

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //scanner to intake information provided by customer
+
+        // Get user input
+        System.out.print("Enter total bill amount ($):");
+        double totalBill = input.nextDouble(); //asks for totalBill, stores in variable
+
+        System.out.print("Enter tip percentage: ");
+        int tipPercentage = input.nextInt(); //asks for tip percentage, stores in a variable
+
+        System.out.print("Enter number of people in your group: ");
+        int numberOfPeople = input.nextInt(); //asks for number of people, stores into a variable
+
+        double tipAmount = totalBill * (tipPercentage / 100.0);   //calculating tip amount
+        double totalWithTip = totalBill + tipAmount;                //total with tip
+        double tipPerPerson = tipAmount / numberOfPeople;           //tip each person has to pay
+        double totalPerPerson = totalWithTip / numberOfPeople;      //total amount each person has to equally pay
+
+
+        //prints out all information in a clean format
+        System.out.println();
+        System.out.println("Total Tip: " +  tipAmount);
+        System.out.println("Total Bill (with Tip): " + totalWithTip);
+        System.out.println("Tip Per Person: " + tipPerPerson);
+        System.out.println("Total Per Person: " + totalPerPerson);
+    }
+}
+
